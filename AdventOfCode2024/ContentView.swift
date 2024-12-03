25ff34742fafc56dc7f7d40fdd78c0a9a2ecef6b
Devy <izabela.marcinkowska@outlook.com>
2024-12-03 12:28:36 +0100

    Day 1

---
+++ AdventOfCode2024/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AdventOfCode2024
+//
+//  Created by Izabela Marcinkowska on 2024-12-03.
+//
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        VStack {
+           Text("Hello")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
